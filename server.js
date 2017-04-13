7644d1764c27b3d1c33722f76580c660ba8c7847
Andrew Cox <aedryan@gmail.com>
2017-04-13 14:26:54 -0500

    Finished adding functionality.

--- server.js
+++ server.js
@@ -1,10 +1,28 @@
-var express = require('express')
-var app = express()
+const path = require("path");
+const express = require('express');
+const app = express();
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 app.set('port', (process.env.PORT || 5000));
+app.use('/favicon.ico', express.static("favicon.ico"));
 
-app.get('/:timestamp', (req, res) => {
-    res.send(req.params);
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get('/:date', (req, res) => {
+    const param = req.params.date;
+    const unixTest = /^-?\d+$/;
+    let result, date;
+
+    if (unixTest.test(param)) {
+        date = new Date(param * 1000);
+    } else if (Object.prototype.toString.call(new Date(param)) === "[object Date]" && !isNaN(new Date(param).getTime())) {
+        date = new Date(param);
+    }
+    console.log(new Date(param));
+    result = { unix: (date ? (+ date / 1000) : null), natural: (date ? (months[date.getMonth()] + " " + date.getDate() + ", " + date.getFullYear()) : null) };
+    res.json(result);
 });
 
 app.listen(app.get('port'), () => {

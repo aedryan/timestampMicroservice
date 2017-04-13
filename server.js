903317aa5d1d5d53df447b8aef466b9b68e345d8
Andrew Cox <aedryan@gmail.com>
2017-04-13 12:51:13 -0500

    Updated handler.

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express()
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/:timestamp', (req, res) => {
-    document.body.innerHTML = req.params;
+    window.document.body.innerHTML = req.params;
 });
 
 app.listen(app.get('port'), () => {
